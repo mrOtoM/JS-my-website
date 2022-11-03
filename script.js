9d7f28b72fa50682908666c14d5a2b6a42b32cc9
mrOtoM <mrotomachala@gmail.com>
2022-11-03 13:03:36 +0100

    Add js code for smooth nnav scrolling

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+document.querySelectorAll('.nav__link').forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const id = this.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
