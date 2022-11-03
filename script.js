b94b13c9e6a2e9709b1b176f7298dd0cdeaf1d17
mrOtoM <mrotomachala@gmail.com>
2022-11-03 13:25:52 +0100

    Add smooth scrolling update to event delegation

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 'use strict';
 
-document.querySelectorAll('.nav__link').forEach(function (el) {
-  el.addEventListener('click', function (e) {
-    e.preventDefault();
+// Page Navbar smooth scrolling
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
 
-    const id = this.getAttribute('href');
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({
+//       behavior: 'smooth',
+//     });
+//   });
+// });
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
     console.log(id);
     document.querySelector(id).scrollIntoView({
       behavior: 'smooth',
     });
-  });
+  }
 });
